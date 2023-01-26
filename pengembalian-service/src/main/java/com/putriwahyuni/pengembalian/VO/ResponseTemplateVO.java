05e893d4774ae043a296685bb2880d3bdc2b1570
Ptrwa <113502703+Ptrwa@users.noreply.github.com>
2023-01-27 06:45:09 +0700

    Add files via upload

---
+++ pengembalian-service/src/main/java/com/putriwahyuni/pengembalian/VO/ResponseTemplateVO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.putriwahyuni.pengembalian.VO;
+import com.putriwahyuni.pengembalian.entity.Pengembalian;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author ASUS
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private Pengembalian pengembalian;
+    private Peminjaman peminjaman;
+}
