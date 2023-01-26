c538e8c9e28abfda275fc36043fe36ebad7d26b9
Ptrwa <113502703+Ptrwa@users.noreply.github.com>
2023-01-27 00:18:07 +0700

    Add files via upload

---
+++ pelanggan-service/src/test/java/com/putri/pelangganservice/PelangganServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putri.pelangganservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelangganServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
